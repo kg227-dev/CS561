b0d0748e692f7bd47f574f78524e4581f717a7d1
Kush <kg227@duke.edu>
2023-02-02 18:26:50 -0500

    added missing files

---
+++ Assignment1/parsing.py
@@ -0,0 +1,20 @@
+import sys,re
+#function that reads FASTA file and returns a dictionary
+def fasta(file):
+    f=open(file,'r')
+    lines=f.readlines()
+    hre=re.compile('>(\S+)')
+    lre=re.compile('^(\S+)$')
+    gene={}
+    for line in lines:
+            outh = hre.search(line)
+            if outh:
+                    id=outh.group(1)
+            else:
+                    outl=lre.search(line)
+                    if(id in gene.keys()):
+                            gene[id] += outl.group(1)
+                    else:
+                            gene[id]=outl.group(1)
+    return gene
+
