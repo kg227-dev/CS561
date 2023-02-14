9ce343387b813725e0ffacd314aedd6bb4047e5c
Sydney Ballard <60205322+slizabeth@users.noreply.github.com>
2023-02-14 16:01:06 -0500

    Changing file structure

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -5,8 +5,19 @@ from linear import *
 from get_alignment_output import *
 
 def main():
+    ############################
+    # PAIRWISE SCORING MATRIX  #
+    ############################
+    # File path to scoring matrix
     matrix_file ="cs561 repository COLLABORATIVE W KUSH/CS561/Assignment1/matrix.txt"
 
+
+    ############################
+    #   SELECTING FILE PATHS   #
+    ############################
+    # File paths ran two at a time:
+    #   - close-first.fasta  + close-second.fasta
+    #   - distant-first.fast + distant-second.fasta
 
     ### CLOSE
     # first_file = "cs561 repository COLLABORATIVE W KUSH/CS561/Assignment1/close-first.fasta"
@@ -15,6 +26,12 @@ def main():
     ### DISTANT
     first_file = "cs561 repository COLLABORATIVE W KUSH/CS561/Assignment1/distant-first.fasta"
     second_file = "cs561 repository COLLABORATIVE W KUSH/CS561/Assignment1/distant-second.fasta"
+
+
+    ############################
+    # SCORING / ALIGNMENT TYPE #
+    ############################
+    # output() function performs bulk of operations, including console output to new file
 
     # Linear scoring: 
     # output(first_file, second_file, "linear-close.txt", matrix_file)
