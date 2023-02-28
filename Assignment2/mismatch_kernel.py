136f5d284a07fd19de861d8d9c905e719c19b5dd
kush <kg227@duke.edu>
2023-02-27 20:21:02 -0500

    added part 2 of assignment 2

---
+++ Assignment2/mismatch_kernel.py
@@ -0,0 +1,37 @@
+import random
+
+
+def mismatch_kernel(seq1, seq2, k):
+    # Compute all k-mers for both sequences
+    kmers1 = [seq1[i:i+k] for i in range(len(seq1)-k+1)]
+    kmers2 = [seq2[i:i+k] for i in range(len(seq2)-k+1)]
+
+    # Initialize similarity score to 0
+    similarity = 0
+
+
+    # Loop over all k-mers in first sequence
+    for kmer1 in kmers1:
+        # Loop over all k-mers in second sequence
+        for kmer2 in kmers2:
+            # Count number of mismatches
+            mismatches = sum([1 for i in range(k) if kmer1[i] != kmer2[i]])
+
+            # Update the similarity score based on the number of mismatches
+            if mismatches <= 1:
+                similarity += 1
+
+    return similarity
+
+
+if __name__ == "__main__":
+    nucleotides = ['A', 'C', 'G', 'T']
+    seq1 = ''.join(random.choice(nucleotides) for i in range(100))
+    seq2 = ''.join(random.choice(nucleotides) for i in range(100))
+
+    k = 3
+    similarity = mismatch_kernel(seq1, seq2, k)
+
+    print("Sequence 1: ", seq1)
+    print("Sequence 2: ", seq2)
+    print("Similarity score (k={}): {}".format(k, similarity))
--- Assignment2/spectrum_kernel.py
+++ Assignment2/spectrum_kernel.py
@@ -3,19 +3,18 @@ import random
 
 
 def spectrum_kernel(seq1, seq2, k):
-    # Generate the set of all possible k-mers
+    # Generate set of all possible k-mers
     kmers = set()
     for i in range(len(seq1) - k + 1):
         kmers.add(seq1[i:i+k])
     for i in range(len(seq2) - k + 1):
         kmers.add(seq2[i:i+k])
 
-    # Create a dictionary mapping k-mers to indices
+    # Create dictionary mapping k-mers to indices
     kmer_dict = {}
     for i, kmer in enumerate(sorted(kmers)):
         kmer_dict[kmer] = i
 
-    print(kmer_dict)
 
     # Convert sequences to feature vectors
     vec1 = seq_to_vec(seq1, kmer_dict, k)
@@ -26,10 +25,10 @@ def spectrum_kernel(seq1, seq2, k):
 
 
 def seq_to_vec(seq, kmer_dict, k):
-    # Initialize a feature vector of zeros
+    # Initialize feature vector
     vec = np.zeros(len(kmer_dict))
 
-    # Populate the feature vector with k-mer counts
+    # Populate feature vector with k-mer counts
     for i in range(len(seq) - k + 1):
         kmer = seq[i:i+k]
         if kmer in kmer_dict:
